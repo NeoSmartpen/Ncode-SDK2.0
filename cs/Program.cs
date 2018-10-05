e8c132f80c77eeaff0fbb4b0fd3e97487eb262f8
HRHWANG <hrhwang@neolab.net>
2018-10-05 17:51:14 +0900

    Initial commit

---
+++ cs/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using NeoLABNcodeSDK;
+using System.IO;
+using System.Diagnostics;
+
+namespace sampleApp_Ncode_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateNcode();
+        }
+
+        static void GenerateNcode()
+        {
+            CNcodeSDK sdk = new CNcodeSDK();
+
+            Console.WriteLine();
+            Console.WriteLine("Ncode SDK version : " + sdk.GetVersion());
+            Console.WriteLine();
+
+            Console.WriteLine("1) Initializing with app key");
+            Console.WriteLine();
+            
+            // this is sample app key for testing
+            if(sdk.Init("juyhgt54redfv7ujmnhgt5esq0poli") == false)
+            {
+                Console.WriteLine("   Error message : " + sdk.GetLastError());
+                Console.ReadLine();
+                return;
+            }
+
+
+            Console.WriteLine("2) Getting tickets list (optional)");
+            Console.WriteLine();
+            List<TicketInfo> tickets = sdk.GetTickets();
+            if(tickets == null)
+            {
+                Console.WriteLine("   Error message : " + sdk.GetLastError());
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("   Found " + tickets.Count + " ticket(s)");
+            Console.WriteLine();
+
+            for (int i = 0; i < tickets.Count; ++i)
+            {
+                Console.WriteLine("   Ticket[" + i.ToString() + "]");
+                if (tickets[i].ncodeType == NCODE_TYPE.N3C6)
+                    Console.WriteLine("   Type    : N3C6");
+                else if (tickets[i].ncodeType == NCODE_TYPE.G3C6)
+                    Console.WriteLine("   Type    : G3C6");
+                else if (tickets[i].ncodeType == NCODE_TYPE.S1C6)
+                    Console.WriteLine("   Type    : S1C6");
+                else if (tickets[i].ncodeType == NCODE_TYPE.P1C6)
+                    Console.WriteLine("   Type    : P1C6");
+                Console.WriteLine("   Section : " + tickets[i].section.ToString());
+                Console.WriteLine("   Owner   : " + tickets[i].ownerStart.ToString() + "~" + (tickets[i].ownerStart + tickets[i].ownerSize - 1).ToString());
+                Console.WriteLine("   Book    : " + tickets[i].bookStart.ToString() + "~" + (tickets[i].bookStart + tickets[i].bookSize - 1).ToString());
+                Console.WriteLine("   Page    : " + tickets[i].pageStart.ToString() + "~" + (tickets[i].pageStart + tickets[i].pageSize - 1).ToString());
+                Console.WriteLine("   Info    : " + tickets[i].extraInfo);
+                Console.WriteLine("   Period  : " + tickets[i].period);
+
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine("3) Choose ticket and set start page (optional)");
+            Console.WriteLine();
+
+            int ticketIndex = 1;
+            int ownerOffset = 0;
+            int bookOffset = 0;
+            int pageOffset = 0;
+            TicketInfo startPageInfo = sdk.SetStartPageFromTicket(tickets[ticketIndex], ownerOffset, bookOffset, pageOffset);
+
+            if (startPageInfo == null)
+            {
+                Console.WriteLine("   Ticket range error");
+                Console.WriteLine("   Error message : " + sdk.GetLastError());
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("   Selected ticket index : " + ticketIndex.ToString());
+            Console.WriteLine("   Owner offset : " + ownerOffset.ToString());
+            Console.WriteLine("   Book offset : " + bookOffset.ToString());
+            Console.WriteLine("   Page offset : " + pageOffset.ToString());
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("4) Set size for inch from paper name (optional)");
+            Console.WriteLine();
+            string paperSizeName = "A4";
+            SizeF pageSize = sdk.GetInchValueFromPaperName(paperSizeName, false);
+            Console.WriteLine("   Paper Size (" + paperSizeName + ") : " + "(" + pageSize.Width.ToString() + ", " + pageSize.Height.ToString() + ")");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("5) Generating Ncode data");
+            Console.WriteLine();
+            List<NcodePage> codeData = new List<NcodePage>();
+            int pageCount = 1;
+
+            if (sdk.GenerateNcode(
+                out codeData,
+                startPageInfo,
+                pageSize.Width,     // inch
+                pageSize.Height,    // inch
+                pageCount) != 0)
+            {
+                Console.WriteLine("   Error message : " + sdk.GetLastError());
+                Console.ReadLine();
+                return;
+            }
+
+            // You can also create Ncode data via entering code informaion directly.
+            // Use it when you do not need to inquiry tickets and you know exactly what code information you need.
+            //
+            //if (sdk.GenerateNcode(
+            //    out codeData,
+            //    CNcodeSDK.NCODE_TYPE.N3C6,  // Ncode type
+            //    3,                          // section
+            //    28,                         // owner
+            //    10,                         // book
+            //    1,                          // page
+            //    6.0,                        // inch
+            //    8.0,                        // inch
+            //    pageCount) != 0)
+            //{
+            //    Console.WriteLine("   Error message : " + sdk.GetLastError());
+            //    Console.ReadLine();
+            //    return;
+            //}
+
+
+
+            Console.WriteLine("5-1) Saving Ncode image file");
+            Console.WriteLine();
+
+            for (int i = 0; i < pageCount; ++i)
+            {
+                string outputFilename = string.Format("{0}_{1}_{2}_{3}_{4}_{5}.png",
+                    codeData[i].section.ToString(),
+                    codeData[i].owner.ToString(),
+                    codeData[i].book.ToString(),
+                    codeData[i].page.ToString(),
+                    pageSize.Width,
+                    pageSize.Height
+                    );
+                if (sdk.GetImage(codeData[i], 600, outputFilename, false) != 0)
+                {
+                    Console.WriteLine("   Error message : " + sdk.GetLastError());
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+
+            /////////////////////////////////////////////////
+            // caution!
+            // Temporarily support only S1C6 and P1C6.
+            // Using N3C6 and G3C6 causes 343 error code.
+            /////////////////////////////////////////////////
+            //Console.WriteLine("5-2) Saving Ncode postscript file");
+            //Console.WriteLine();
+
+            //{
+            //    string outputFilename = string.Format("{0}_{1}_{2}_{3}-{4}.ps",
+            //            codeData[0].section.ToString(),
+            //            codeData[0].owner.ToString(),
+            //            codeData[0].book.ToString(),
+            //            codeData[0].page.ToString(),
+            //            codeData.Count - 1);
+            //    if (sdk.GetPostscript(codeData, outputFilename) != 0)
+            //    {
+            //        Console.WriteLine("   Error message : " + sdk.GetLastError());
+            //        Console.WriteLine("   Error message : Temporarily support only S1C6 and P1C6.");
+            //        Console.ReadLine();
+            //        return;
+            //    }
+
+            //    Console.WriteLine("5-2-1) Converting postscript to PDF via Ghostscript");
+            //    Console.WriteLine();
+
+            //    Process process = new System.Diagnostics.Process();
+            //    process.StartInfo.FileName = "cmd.exe";
+            //    process.StartInfo.Arguments = "/C ps2pdf " + outputFilename;
+            //    process.Start();
+            //}
+
+            Console.WriteLine("6) Complete");
+            Console.WriteLine("   press a key");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,74 @@
+## Ncode™ SDK
+
+Ncode™ SDK for printing Ncode patterns. This open-source library allows you to apply Ncode™ on your paper forms.
+
+## About Neo smartpen
+
+The Neo smartpen is designed to seamlessly integrate the real and digital worlds by transforming what you write on paper - everything from sketches and designs to business meeting notes - to your iOS, Android and Windows devices. It works in tandem with N notebooks, powered by NeoLAB Convergence’s patented Ncode™ technology and the accompanying application, Neo Notes. Find out more at www.neosmartpen.com
+
+## About Ncode™: service development guide 
+
+‘Natural Handwriting’ technology based on Ncode™(Microscopic data patterns containing various types of data) is a handwriting stroke recovery technology that digitizes paper coordinates obtained by optical pen devices such as Neo smartpen. The coordinates then can be used to store handwriting stroke information, analyzed to extract meaning based on user preferences and serve as the basis for many other types of services. 
+
+Click the link below to view a beginners guide to Ncode technology. 
+<< [https://github.com/NeoSmartpen/Documentations/blob/master/Ncode™ Service Development Getting Started Guide v1.01.pdf](https://github.com/NeoSmartpen/Documentations/blob/master/Ncode%E2%84%A2%20Service%20Development%20Getting%20Started%20Guide%20v1.01.pdf) >>
+
+## Introduction
+
+This is a C# and C++ sample project for printing Ncode™ image. And also included SDK library.
+ 
+## LICENSE
+
+Ncode™ SDK Copyright (c) 2017 NeoLAB Convergence, Inc.
+
+Ncode™ SDK is required purchase commercial license.
+Ncode™ SDK includes a limited test key, you can develop the prototype software without the commercial license.
+
+Please contact following to get more information:
+   - Global: _global1@neolab.net
+   - Korea: _biz1@neolab.net
+
+-------------
+And we are using the following open sources.
+#### NewtonJSON (https://github.com/JamesNK/Newtonsoft.Json/blob/master/LICENSE.md)
+
+The MIT License (MIT)
+
+Copyright (c) 2007 James Newton-King
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#### png++ (https://www.nongnu.org/pngpp/license.html)
+
+Copying png++ is subject to the following license:
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+   3. The name of the author may not be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+---
+**Feel free to leave any comment or feedback [here](https://github.com/NeoSmartpen/Ncode-SDK/issues)**
